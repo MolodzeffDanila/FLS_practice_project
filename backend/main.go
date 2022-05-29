f59fabb8076865a50127795126d43b6cfc76c04e
muratom <muratom73@gmail.com>
2022-05-29 23:54:06 +0300

    Refactored CORS code

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,6 @@ import (
 	"backend/db"
 	_ "backend/docs"
 	"backend/routers"
-	"github.com/gin-gonic/gin"
 )
 
 // @title World Bank GDP API
@@ -19,22 +18,5 @@ import (
 func main() {
 	db.Init()
 	router := routers.CreateRouters()
-	router.Use(CORSMiddleware())
 	router.Run(":8080")
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
